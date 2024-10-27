0db6af147e9b828dbfc7d4d9e7dd7a6a931e6cfa
My Chiffon Nguyen <chiffonng136@gmail.com>
2024-10-26 23:24:12 -0700

    Switch to uv for Python package and project management

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# https://docs.astral.sh/uv/getting-started/features/
+
+# If uv is available, use it to install the project
+if command -v uv &> /dev/null
+then
+    uv venv
+    source .venv/bin/activate
+    uv pip install -r pyproject.toml -e .
+    uv sync
+else
+    python3.12 -m venv venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
+fi
+
+# Upgrade
+# uv tool upgrade --all
+
+# Run
+# uv run main.py
+
