fd18d23207a321b688fcd229016e2cc7a64cf9cc
My Nguyen <46087525+chiffonng@users.noreply.github.com>
2024-10-27 21:56:36 +0100

    #3 Classify mnemonics with OpenAI API
    * Add mnemonic examples to README
    
    * Refactor .gitignore and data_processing.py
    
    - Refactor .gitignore to exclude /temp directory and ignore all .parquet and .csv files.
    - Format mnemonics more consistently
    - Drop mnemonics with only 2 words or less.
    
    * Separate prompts storage to YAML files
    
    * Refactor prompt config and mnemonic processing module
    
    * Switch to uv for Python package and project management
    
    * Fix imports between Python modules inside src
    
    * Relax .python-version and add more instructions for installation and API keys
    
    * Add pre-commit and its configurations
    
    * Refactor file paths and add error handling module
    
    * Improve mnemonics classification prompts and instructions
    
    * Sync dependencies in requirements.txt with virtual env
    
    * Extend error handling and extend ruff safe-fixes in pyproject.toml
    
    * Improve standardization of classification results.
    
    - Add JSON schema as response format for OpenAI API
    - Handle errors when OpenAI response is too long/short
    - Refactor function classify_mnemonics_api
    - Improve logging
    
    * Fix lint workflow

---
+++ src/constants.py
@@ -0,0 +1,14 @@
+"""Module for constant variables across the project."""
+
+PARQUET_EXT = ".parquet"
+CSV_EXT = ".csv"
+TXT_EXT = ".txt"
+
+RAW_DATA_DIR = "data/raw"
+PROCESSED_DATA_DIR = "data/processed"
+COMBINED_DATASET_CSV = PROCESSED_DATA_DIR + "/combined.csv"
+COMBINED_DATASET_PARQUET = PROCESSED_DATA_DIR + "/combined.parquet"
+CLASSIFIED_DATASET_CSV = PROCESSED_DATA_DIR + "/classified.csv"
+CLASSIFIED_DATASET_PARQUET = PROCESSED_DATA_DIR + "/classified.parquet"
+FINAL_DATASET_CSV = "data/final.csv"
+FINAL_DATASET_PARQUET = "data/final.parquet"
