c2fd8052dd982a75722dc408ef747989f6d6e0a2
My Chiffon Nguyen <chiffonng136@gmail.com>
2024-10-31 13:45:36 -0700

    Add wandb as dependency and fix optional installations

--- setup.sh
+++ setup.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 # https://docs.astral.sh/uv/getting-started/features/
 
+# Catch --optional flag
+if [ "$1" == "--optional" ]; then
+    echo "Optional dependencies will be installed."
+    OPTIONAL=true
+else
+    OPTIONAL=false
+fi
+
 # If uv is available, use it to install the project
 if command -v uv &> /dev/null
 then
     uv venv
     # shellcheck source=.venv/bin/activate
     source .venv/bin/activate
-    uv pip install -r pyproject.toml -e .
+    if [ "$OPTIONAL" = true ]; then
+        uv pip install -r pyproject.toml -e . --all-extras
+    else
+        uv pip install -r pyproject.toml -e .
+    fi
+
 else
-    python3.12 -m venv venv
+    python3 -m venv venv
     # shellcheck source=.venv/bin/activate
     source .venv/bin/activate
-    pip install -r requirements.txt
+    if [ "$OPTIONAL" = true ]; then
+        pip install -e .[all]
+    else
+        pip install -e .
+    fi
 fi
 
 # Upgrade
@@ -27,5 +44,4 @@ if [ ! -f .env ]; then
 fi
 
 # Create project directories
-mkdir -p data
 mkdir -p logs
