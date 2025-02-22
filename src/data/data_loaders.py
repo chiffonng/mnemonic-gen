5b80c8476a9e5c923b82f773179b3173ae0dbab7
My Nguyen <46087525+chiffonng@users.noreply.github.com>
2025-02-22 02:57:44 +0100

    Relocate data and app module (#13)
    * Move data module out of "src"
    * Move app module out of "src" and store on HuggingFace Space instead.
    * Fix import redundancies

--- src/data/data_loaders.py
+++ src/data/data_loaders.py
@@ -11,9 +11,9 @@ if TYPE_CHECKING:
     from datasets import Dataset
 
 from src.utils import constants as c
-from src.utils.aliases import ExtensionsType, PathLike
+from src.utils.aliases import PathLike
 from src.utils.common import login_hf_hub
-from src.utils.error_handling import check_dir_path, check_file_path
+from src.utils.error_handling import check_file_path
 
 # Set up logging to console
 logger = logging.getLogger(__name__)
@@ -110,7 +110,4 @@ def load_hf_dataset(
 if __name__ == "__main__":
     # Load a dataset from the Hugging Face hub
     mnemonic_dataset: "Dataset" = load_hf_dataset()
-    test_dataset: "Dataset" = load_hf_dataset(
-        repo_id="nbalepur/Mnemonic_Test", split="train"
-    )
     logger.info(f"\n\n{mnemonic_dataset}")
