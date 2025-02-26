776b1b53b875ff004565669fa777c572c11bd013
My Chiffon Nguyen <chiffonng136@gmail.com>
2025-02-26 04:41:58 -0800

    Create data_prep module

---
+++ data_prep/__init__.py
@@ -0,0 +1,9 @@
+"""Module for data pipeline.
+
+1. data_processing.py: Process data by ingesting, cleaning, standardizing, transforming, splitting and exporting it.
+2. mnemonic_processing.py: Clean, standardize, and diversify data, using OpenAI fine-tuning API.
+3. test_data_processing.py: Generate test data, ensure it is clean, consistent, and non-duplicative.
+
+Outside of data module:
+1. src/data_loaders.py: Load data from local and HuggingFace Hub.
+"""
