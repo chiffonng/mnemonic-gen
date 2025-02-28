db21aee994f4095d97d4c03c3afcec00a6c1ad3f
My Chiffon Nguyen <chiffonng136@gmail.com>
2025-02-28 14:03:12 -0800

    Upload validation file for OpenAI supervised finetuning

--- data_prep/mnemonic_processing.py
+++ data_prep/mnemonic_processing.py
@@ -8,11 +8,14 @@ import csv
 import json
 import logging
 import random
-from collections import defaultdict
 from typing import TYPE_CHECKING
 
 from dotenv import load_dotenv
-from src.openai.openai_ft import finetune_from_config, upload_file_to_openai
+from src.openai.openai_ft import (
+    finetune_from_config,
+    upload_file_to_openai,
+    validate_openai_file,
+)
 from src.utils import check_file_path, read_config, read_prompt, update_config
 
 from openai import OpenAI
@@ -47,7 +50,7 @@ logger.handlers[0].setFormatter(
 
 
 def prepare_and_split_finetune_data(
-    input_data: "Path" = raw_input_path,
+    input_path: "Path" = raw_input_path,
     input_prompt: "Path" = prompt_path,
     output_train_jsonl: "Path" = train_input_path,
     output_val_jsonl: "Optional[Path]" = None,
@@ -56,7 +59,7 @@ def prepare_and_split_finetune_data(
     """Prepare the data (JSONL) for fine-tuning with OpenAI's API and split into training and validation files.
 
     Args:
-        input_data (Path): The path to the input data.
+        input_path (Path): The path to the input data.
         input_prompt (Path): The path to the system prompt.
         output_train_jsonl (Path): The path to save the training data.
         output_val_jsonl (Path): The path to save the validation data. If None, the validation data will not be saved.
@@ -69,6 +72,9 @@ def prepare_and_split_finetune_data(
         ValueError: If the split ratio is invalid.
     """
     if output_val_jsonl is None:
+        logger.info(
+            "There is no validation data path provided. Validation data will not be saved."
+        )
         split_ratio = 1
 
     if not isinstance(split_ratio, float):
@@ -81,7 +87,7 @@ def prepare_and_split_finetune_data(
     system_prompt = read_prompt(input_prompt)
 
     # Read all rows from CSV into a list of dictionaries
-    with input_data.open("r", encoding="utf-8") as infile:
+    with input_path.open("r", encoding="utf-8") as infile:
         reader = csv.DictReader(infile)
         rows = list(reader)
 
@@ -96,11 +102,12 @@ def prepare_and_split_finetune_data(
     val_rows = rows[int(num_examples * split_ratio) :] if split_ratio < 1 else None
 
     # Write the training and if not None, the validation data to JSONL files.
+    logger.debug("Processing training data...")
     with output_train_jsonl.open("w", encoding="utf-8") as train_file:
         for row in train_rows:
             data = {
                 "messages": [
-                    {"role": "developer", "content": system_prompt},
+                    {"role": "system", "content": system_prompt},
                     {
                         "role": "user",
                         "content": f"Term: {row['term'].strip()}\nCurrent mnemonic: {row['mnemonic'].strip()}",
@@ -111,11 +118,12 @@ def prepare_and_split_finetune_data(
             train_file.write(json.dumps(data) + "\n")
 
     if output_val_jsonl is not None and val_rows is not None:
+        logger.debug("Processing validation data...")
         with output_val_jsonl.open("w", encoding="utf-8") as val_file:
             for row in val_rows:
                 data = {
                     "messages": [
-                        {"role": "developer", "content": system_prompt},
+                        {"role": "system", "content": system_prompt},
                         {
                             "role": "user",
                             "content": f"Term: {row['term'].strip()}\nCurrent mnemonic: {row['mnemonic'].strip()}",
@@ -131,97 +139,6 @@ def prepare_and_split_finetune_data(
     logger.info(
         f"Data prepared ({num_examples} examples) and split into training and validation files.\nTraining data path: {output_train_jsonl}, \nValidation data path: {output_val_jsonl}"
     )
-
-
-# TODO: Move it to openai_ft.py, or another module.
-def validate_finetune_data(input_data: "Path") -> None:
-    """Validate the data for fine-tuning with OpenAI's API. Source code from OpenAI Cookbook: https://cookbook.openai.com/examples/chat_finetuning_data_prep.
-
-    Args:
-        input_data (Path): The path to the input data. The data should be in JSONL format.
-
-    Returns:
-        None
-    """
-    input_data = check_file_path(input_data, extensions=["jsonl"])
-
-    with input_data.open("r", encoding="utf-8") as f:
-        dataset = [json.loads(line) for line in f]
-
-    # Format error checks
-    format_errors: defaultdict = defaultdict(int)
-
-    for ex in dataset:
-        if not isinstance(ex, dict):
-            format_errors["data_type"] += 1
-            continue
-
-        messages = ex.get("messages", None)
-        if not messages:
-            format_errors["missing_messages_list"] += 1
-            continue
-
-        for message in messages:
-            # Check that 'role' is present and at least one of 'content' or 'tool' is provided.
-            if "role" not in message or (
-                "content" not in message and "tool" not in message
-            ):
-                format_errors["message_missing_key"] += 1
-
-            # Validate that all keys in the message are recognized.
-            if any(
-                k
-                not in (
-                    "role",
-                    "content",
-                    "name",
-                    "weight",
-                    "refusal",
-                    "audio",
-                    "tool_calls",
-                    "tool_call_id",
-                )
-                for k in message
-            ):
-                format_errors["message_unrecognized_key"] += 1
-            if message.get("role", None) not in (
-                "system",
-                "user",
-                "assistant",
-                "tool",
-            ):
-                format_errors["unrecognized_role"] += 1
-
-            # Check that either 'content' or 'tool' exists and that 'content', if present, is a string.
-            content = message.get("content")
-            tool = message.get("tool")
-            if (content is None and tool is None) or (
-                content is not None and not isinstance(content, str)
-            ):
-                format_errors["missing_content"] += 1
-
-        # Each example should have at least one assistant message.
-        if not any(msg.get("role") == "assistant" for msg in messages):
-            format_errors["example_missing_assistant_message"] += 1
-
-        tools = ex.get("tools", None)
-        if tools:
-            for tool in tools:
-                if not isinstance(tool, dict):
-                    format_errors["tool_data_type"] += 1
-                    continue
-                if any(k not in ("type", "function") for k in tool):
-                    format_errors["tool_unrecognized_key"] += 1
-
-    if format_errors:
-        logger.error("Errors found in formatting fineturning data:")
-        for k, v in format_errors.items():
-            logger.error(f"{k}: {v}")
-    else:
-        logger.info(
-            "Data is formatted ROUGHLY correctly. Always check the data manually with the OpenAI API reference here: https://platform.openai.com/docs/api-reference/fine-tuning/chat-input."
-        )
-        logger.info(f"Number of examples: {len(dataset)}")
 
 
 def upload_finetune_data(
@@ -278,7 +195,7 @@ def upload_finetune_data(
     return None
 
 
-# TODO: Add a function to validate the config file, or update src.utils.common.read_conf to validate the config file.
+# TODO: Add a function to validate the config file, or update src.utils.common.read_config to validate the config file.
 
 
 def _get_cached_file_id(
@@ -344,13 +261,16 @@ def _get_cached_file_id(
 
 
 if __name__ == "__main__":
-    prepare_and_split_finetune_data()
-    validate_finetune_data(train_input_path)
-    validate_finetune_data(val_input_path)
+    prepare_and_split_finetune_data(
+        output_train_jsonl=train_input_path,
+        output_val_jsonl=val_input_path,
+    )
+    validate_openai_file(train_input_path)
+    validate_openai_file(val_input_path)
 
     load_dotenv()
     client = OpenAI()
-    # upload_finetune_data(client, input_path=train_input_path)
-    # update_finetune_data(client, input_path=val_input_path)
+    # upload_finetune_data(client, input_path=train_input_path, file_type="train")
+    # upload_finetune_data(client, input_path=val_input_path, file_type="validation")
 
-    # finetune_from_config(client, config_file_path, finetune_model_id_path)
+    finetune_from_config(client, config_file_path, finetune_model_id_path)
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,2 @@
 from .common import read_config, read_prompt, update_config
-from .error_handling import check_dir_path, check_file_path,check_extension, ExplicitEnum
+from .error_handling import check_dir_path, check_file_path,check_extension, ExplicitEnum, which_file_exists
