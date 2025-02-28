8d2ad2d5d3aa0f8eded9550a92bb458702a96de0
My Chiffon Nguyen <chiffonng136@gmail.com>
2025-02-28 03:27:50 -0800

    Add functions to read and update prompts and configuration files

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,8 +1,13 @@
 """Module for common utility functions."""
 
+import json
 from typing import TYPE_CHECKING
 
+from src.utils.error_handling import check_file_path
+
 if TYPE_CHECKING:
+    from pathlib import Path
+
     from src.utils.aliases import PathLike
 
 
@@ -32,7 +37,23 @@ def get_hf_token():
     return os.getenv("HF_TOKEN")
 
 
-def read_conf(conf_path: "PathLike") -> dict:
+def read_prompt(prompt_path: "Path") -> str:
+    """Read the system prompt from a .txt file.
+
+    Args:
+        prompt_path (Path): The path to the prompt file.
+
+    Returns:
+        str: The prompt.
+    """
+    prompt_path = check_file_path(prompt_path, extensions=["txt"])
+
+    with prompt_path.open("r") as file:
+        prompt = file.read().strip()
+    return prompt
+
+
+def read_config(conf_path: "PathLike") -> dict:
     """Read a configuration file.
 
     Args:
@@ -41,12 +62,26 @@ def read_conf(conf_path: "PathLike") -> dict:
     Returns:
         dict: The configuration.
     """
-    import json
-
-    from src.utils.error_handling import check_file_path
-
     # Convert to Path object, ensure the file path exists and has the correct extension
     conf_path_obj = check_file_path(conf_path, extensions=["json"])
 
     with conf_path_obj.open("r") as file:
         return json.load(file)
+
+
+def update_config(config_filepath: "PathLike", key: str, new_value: str):
+    """Update the config file with the new_value for the key.
+
+    Args:
+        config_filepath (PathLike): The path to the config file. The file should be in JSON format.
+        key (str): The key to update.
+        new_value (str): The new value to set for the key.
+    """
+    config_file_path: "Path" = check_file_path(config_filepath, extensions=["json"])
+    try:
+        config_data: dict = read_config(config_file_path)
+        config_data[key] = new_value
+        with config_file_path.open("w", encoding="utf-8") as f:
+            json.dump(config_data, f)
+    except Exception as e:
+        raise e
