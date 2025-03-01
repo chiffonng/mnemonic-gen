e70828601cf1b93cba0908500684517718780072
My Chiffon Nguyen <chiffonng136@gmail.com>
2025-03-01 04:36:12 -0800

    Increase max completion tokens and refactor OpenAI utility functions

--- src/openai/__init__.py
+++ src/openai/__init__.py
@@ -1,2 +1,3 @@
-from .openai_ft import upload_file_to_openai, finetune_from_config, validate_openai_file
+from .openai_ft import finetune_from_config
 from .openai_cc import openai_generate_completion, improve_mnemonic, batch_improve_mnemonics
+from .openai_utils import upload_file_to_openai, validate_openai_file, validate_openai_config
