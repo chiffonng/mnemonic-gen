504997e0759ba44ba067036d1767369012457bf9
My Chiffon Nguyen <chiffonng136@gmail.com>
2025-03-14 16:40:31 -0700

    Include pre-merge-commit hooks and clean up requirements

--- scripts/setup-dev.sh
+++ scripts/setup-dev.sh
@@ -6,5 +6,5 @@ uv pip install --upgrade pip
 uv sync # install dev dependencies by default
 
 echo "Update pre-commit hooks..."
-pre-commit install --hook-type pre-commit --hook-type pre-push
+pre-commit install --hook-type pre-commit --hook-type pre-push --hook-type pre-merge-commit
 pre-commit autoupdate
