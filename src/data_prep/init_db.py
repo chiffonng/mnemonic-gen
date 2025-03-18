d01fab974fc7c2b9ec5d4b8587324085abcdde97
My Chiffon Nguyen <chiffonng136@gmail.com>
2025-03-18 16:28:47 -0700

    chore(database): update database uri

--- src/data_prep/init_db.py
+++ src/data_prep/init_db.py
@@ -1,6 +1,11 @@
 """Initialize SQL engine and create tables for the database."""
 
+from pathlib import Path
+
 from sqlmodel import SQLModel, create_engine
 
-engine = create_engine("sqlite:///mnemonics.db")
+from src import const
+
+data_dir = Path(const.PROCESSED_DATA_DIR).resolve()
+engine = create_engine(f"sqlite:///{data_dir}/mnemonics.db")
 SQLModel.metadata.create_all(engine)
