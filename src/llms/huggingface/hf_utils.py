9462f0d6d8b791d73ceb5cc894282c963792ff92
My Chiffon Nguyen <chiffonng136@gmail.com>
2025-03-29 01:21:04 -0700

    fix(hf_utils): update login_hf_hub to accept kwargs

--- src/llms/huggingface/hf_utils.py
+++ src/llms/huggingface/hf_utils.py
@@ -1,15 +1,15 @@
 """Module for common utility functions for interacting with HuggingFace."""
 
 
-def login_hf_hub():
-    """Login to the Hugging Face hub. See documentation: https://huggingface.co/docs/huggingface_hub/en/package_reference/authentication."""
+def login_hf_hub(**kwargs):
+    """Login to the Hugging Face hub. See documentation: https://huggingface.co/docs/huggingface_hub/en/package_reference/authentication.
+
+    Args:
+        **kwargs: Additional keyword arguments to pass to the `login` function from the `huggingface_hub` package.
+    """
     from huggingface_hub import login
 
-    hf_access_token = get_hf_token()
-    login(
-        token=hf_access_token,
-        add_to_git_credential=True,
-    )
+    login(add_to_git_credential=True, **kwargs)
 
 
 def get_hf_token() -> str:
