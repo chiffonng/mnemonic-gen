5dfa00a79e0ae181a1fe44b4b08023273f630739
My Chiffon Nguyen <chiffonng136@gmail.com>
2025-04-04 03:41:30 -0700

    feat: format, convert, and upload mnemonic datasets to HuggingFace

--- src/data_prep/data_converters.py
+++ src/data_prep/data_converters.py
@@ -8,38 +8,38 @@ Main functions:
 
 from __future__ import annotations
 
-import json
-import re
 from typing import TYPE_CHECKING
 
-from datasets import ClassLabel, Dataset, DatasetDict, Features, Value
-from src.data_gen.models import LinguisticFeature
+from datasets import ClassLabel, Dataset, DatasetDict, Features, Value, load_dataset
+from structlog import getLogger
+
+from src.data_gen.models import LinguisticFeature, Mnemonic
 from src.data_prep.data_hf import (
     load_local_dataset,
     push_data_to_hf,
 )
 from src.utils import constants as const
 from src.utils.common import read_prompt
-from structlog import getLogger
 
 if TYPE_CHECKING:
     from typing import Any, Optional
 
-    from src.utils.types import PathLike
     from structlog.stdlib import BoundLogger
+
+    from src.utils.types import PathLike
 
 # Set up logging
 logger: BoundLogger = getLogger(__name__)
 
 
 # TODO: remove the function after refactoring
-def create_hf_mnemonic_dataset(
+def create_class_dataset(
     input_path: PathLike,
     select_col_names: str | list[str] = None,
     val_ratio: float = 0.2,
     seed: int = 42,
 ) -> DatasetDict:
-    """Create a HuggingFace dataset from mnemonic data.
+    """Create a HuggingFace dataset where there are clearer class labels for the mnemonics.
 
     Args:
         input_path (PathLike): Path to the input data (filepath or SQLite)
@@ -56,22 +56,9 @@ def create_hf_mnemonic_dataset(
     num_mnemonic_types = len(mnemonic_type_labels)
 
     # Column names to select
-    select_col_names = select_col_names or [
-        "term",
-        "mnemonic",
-        "linguistic_reasoning",
-        "main_type",
-        "sub_type",
-    ]
+    select_col_names = select_col_names or Mnemonic.model_fields.keys()
 
     dataset: Dataset = load_local_dataset(input_path)
-    # Change "mnemonic" column to the content of the "improved_mnemonic" column if it exists
-    if "improved_mnemonic" in dataset.column_names:
-        dataset = dataset.map(
-            lambda x: {"mnemonic": x["improved_mnemonic"]},
-            remove_columns=["improved_mnemonic"],
-        )
-        logger.debug("Replaced 'mnemonic' with 'improved_mnemonic'")
 
     dataset = dataset.select_columns(select_col_names)
     logger.debug("Loaded dataset columns", columns=dataset.column_names)
@@ -81,7 +68,7 @@ def create_hf_mnemonic_dataset(
             "term": Value("string"),
             "mnemonic": Value("string"),
             "reasoning": Value("string"),
-            "main_type": ClassLabel(
+            "linguistic_feature": ClassLabel(
                 names=mnemonic_type_labels, num_classes=num_mnemonic_types
             ),
         }
@@ -90,7 +77,21 @@ def create_hf_mnemonic_dataset(
 
     # Split into train and validation
     splits: DatasetDict = dataset.train_test_split(
-        test_size=val_ratio, stratify_by_column="main_type", seed=seed
+        test_size=val_ratio, stratify_by_column="linguistic_feature", seed=seed
     )
 
     return DatasetDict({"train": splits["train"], "val": splits["test"]})
+
+
+# TODO: convert to chat format {"role": "system", "content": "...", "role": "user", "content": "..."}
+
+# TODO: convert to grpo, conversational format: columns prompts and completions will be lists of message dictionaries.
+ds = load_dataset(const.HF_CONST.DATASET_NAME)
+print(ds)
+# DatasetDict(
+#     {
+#         train: Dataset(
+#             {features: ["term", "instruction", "reasoning", "solution"], num_rows: 1870}
+#         )
+#     }
+# )
