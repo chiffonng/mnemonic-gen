1f1fcd5571f39bf82652dd572ea59043ef8780c8
My Chiffon Nguyen <chiffonng136@gmail.com>
2025-04-21 02:52:28 -0700

    🔨 chore: setup script with conda environment and package install

--- setup.sh
+++ setup.sh
@@ -7,3 +7,8 @@ mkdir -p data
 
 # Clone .env file
 copy .env.template .env
+
+# Create conda environment
+conda env create -n mnemonic-gen python=3.11 torch==2.6.0
+conda activate mnemonic-gen
+uv pip install -r pyproject.toml -e .
